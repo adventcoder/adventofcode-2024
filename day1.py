308721348ad570df47428ade1843a85b0860885a
adventcoder <59347361+adventcoder@users.noreply.github.com>
2024-12-01 13:11:03 +0000

    unengineered day 1

--- day1.py
+++ day1.py
@@ -1,8 +1,10 @@
-import aoc
+from aoc import get_input
+from collections import Counter
 
-@aoc.puzzle()
-def part1(input):
-    return None
+xs, ys = zip(*[map(int, line.split()) for line in get_input(1).splitlines()])
 
-if __name__ == '__main__':
-    aoc.main()
+print('1.', sum(abs(x - y) for x, y in zip(sorted(xs), sorted(ys))))
+
+x_counts = Counter(xs)
+y_counts = Counter(ys)
+print('2.', sum(n*x_counts[n]*y_counts[n] for n in x_counts.keys() & y_counts.keys()))
