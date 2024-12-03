d3eb8ea674793eced2b25c46f065d1fb2acee5d7
adventcoder <59347361+adventcoder@users.noreply.github.com>
2024-12-03 18:09:00 +0000

    shorter

--- day3.py
+++ day3.py
@@ -2,21 +2,10 @@ from aoc import get_input, submit
 import re
 
 def eval(s):
-    return sum(int(a)*int(b) for a, b in re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', s))
+    return sum(int(a)*int(b) for a, b in re.findall(r'mul\(([0-9]+),([0-9]+)\)', s))
 
 def eval_with_conditionals(s):
-    total = 0
-    enabled = True
-    for s in re.findall(r'don\'t\(\)|do\(\)|mul\(\d{1,3},\d{1,3}\)', s):
-        if s.startswith('don\'t'):
-            enabled = False
-        elif s.startswith('do'):
-            enabled = True
-        elif s.startswith('mul'):
-            if enabled:
-                a, b = s[4:-1].split(',')
-                total += int(a) * int(b)
-    return total
+    return eval(re.sub(r"don't\(\)(.*?)do\(\)", '', s))
 
 input = get_input(3)
 submit(eval(input))
