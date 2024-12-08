54b0878b687d359d00dfd2e40f92fcff01b1b398
adventcoder <59347361+adventcoder@users.noreply.github.com>
2024-12-08 21:43:52 +0000

    day7

--- day7.py
+++ day7.py
@@ -1,5 +1,17 @@
 from aoc import get_input, submit
 
+ops = {
+    '+': lambda x, y: x + y,
+    '*': lambda x, y: x * y,
+    '||': lambda x, y: int(str(x) + str(y))
+}
+
+inv_ops = {
+    '+': lambda target, y: target - y,
+    '*': lambda target, y: target // y if target % y == 0 else None,
+    '||': lambda target, y: int(str(target).removesuffix(str(y))) if y != target and str(target).endswith(str(y)) else None
+}
+
 def parse(input):
     eqns = []
     for line in input.splitlines():
@@ -7,27 +19,29 @@ def parse(input):
         eqns.append((int(lhs), list(map(int, rhs.split()))))
     return eqns
 
-def add_inv(target, y): # x + y = target
-    return target - y
+def solvable(target, xs, opnames):
+    lower = [xs[0]]
+    upper = [xs[0]]
+    for x in xs[1:]:
+        lower.append(min(ops[opname](lower[-1], x) for opname in opnames))
+        upper.append(max(ops[opname](upper[-1], x) for opname in opnames))
 
-def mul_inv(target, y): # x * y = target
-    return target // y if target % y == 0 else None
+    def recur(target, i):
+        if not lower[i] <= target <= upper[i]:
+            return False
+        if i == 0:
+            return xs[0] == target
+        for opname in opnames:
+            new_target = inv_ops[opname](target, xs[i])
+            if new_target is not None and recur(new_target, i - 1):
+                return True
+        return False
 
-def cat_inv(target, y):
-    return mul_inv(target - y, 10**len(str(y)))
+    return recur(target, len(xs) - 1)
 
-def solvable(target, xs, inv_ops):
-    if len(xs) == 1:
-        return xs[0] == target
-    else:
-        # if upper_bound(xs, ops) < target:
-        #     return False
-        return any(solvable(new_target, xs[:-1], inv_ops) for inv_op in inv_ops if (new_target := inv_op(target, xs[-1])) is not None)
-
-def total_calibration_result(eqns, inv_ops):
-    return sum(target for target, xs in eqns if solvable(target, xs, inv_ops))
+def total_calibration_result(eqns, opnames):
+    return sum(target for target, xs in eqns if solvable(target, xs, opnames))
 
 eqns = parse(get_input(7))
-# ops that produce a larger output come first to minimize branching
-submit(total_calibration_result(eqns, [mul_inv, add_inv]))
-submit(total_calibration_result(eqns, [cat_inv, mul_inv, add_inv]))
+submit(total_calibration_result(eqns, ['+', '*']))
+submit(total_calibration_result(eqns, ['+', '*', '||']))
